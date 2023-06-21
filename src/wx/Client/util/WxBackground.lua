e2c63cdd6d710d1814bf75a3fd45b0db1d126db5
OttoHatt <ottohatt@gmail.com>
2023-06-21 14:55:15 +0100

    Support literals in WxBackground

--- src/wx/Client/util/WxBackground.lua
+++ src/wx/Client/util/WxBackground.lua
@@ -27,28 +27,35 @@ function WxBackground.Polymorphic(parent, topValue)
 			maid._gradient.Parent = parent
 		end
 
-		maid:GiveTask(
-			Blend.toPropertyObservable(topValue):Subscribe(function(value: { Color3 } | ColorSequence | Color3 | nil)
-				if typeof(value) == "table" then
-					parent.BackgroundColor3 = Color3.new(1, 1, 1)
+		local function handleValue(value: { Color3 } | ColorSequence | Color3 | nil)
+			if typeof(value) == "table" then
+				parent.BackgroundColor3 = Color3.new(1, 1, 1)
 
-					-- TODO: Support >2 colors!
-					if #value == 2 then
-						ensureGradient()
-						maid._gradient.Color = ColorSequence.new(value[1], value[2])
-					end
-				elseif typeof(value) == "ColorSequence" then
-					parent.BackgroundColor3 = Color3.new(1, 1, 1)
+				-- TODO: Support >2 colors!
+				if #value == 2 then
 					ensureGradient()
-					maid._gradient.Color = value
-				elseif typeof(value) == "Color3" then
-					maid._gradient = nil
-					parent.BackgroundColor3 = value
-				else
-					maid._gradient = nil
+					maid._gradient.Color = ColorSequence.new(value[1], value[2])
 				end
-			end)
-		)
+			elseif typeof(value) == "ColorSequence" then
+				parent.BackgroundColor3 = Color3.new(1, 1, 1)
+				ensureGradient()
+				maid._gradient.Color = value
+			elseif typeof(value) == "Color3" then
+				maid._gradient = nil
+				parent.BackgroundColor3 = value
+			else
+				maid._gradient = nil
+			end
+		end
+
+		local observable = Blend.toPropertyObservable(topValue)
+		if observable then
+			maid:GiveTask(observable:Subscribe(handleValue))
+		else
+			-- If we can't convert to an observable, this will be some kind of literal.
+			-- Just apply it, and hope we've got the correct type.
+			handleValue(topValue)
+		end
 
 		return maid
 	end)
