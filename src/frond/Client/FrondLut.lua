74ae58b18ac5d59ec7509a8a0a48f6d64ede4039
OttoHatt <ottohatt@gmail.com>
2023-10-29 10:17:14 +0000

    Small bugbears

--- src/frond/Client/FrondLut.lua
+++ src/frond/Client/FrondLut.lua
@@ -3,8 +3,4 @@ local require = require(script.Parent.loader).load(script)
 local RefCountedLut = require("RefCountedLut")
 local FlexFrondNode = require("FlexFrondNode")
 
-local globalFrondLut = RefCountedLut.new(function()
-	return FlexFrondNode.new()
-end)
-
-return globalFrondLut
+return RefCountedLut.new(FlexFrondNode.new)
--- src/frond/Client/FrondManagedUtils.lua
+++ src/frond/Client/FrondManagedUtils.lua
@@ -7,23 +7,20 @@
 local require = require(script.Parent.loader).load(script)
 
 local FlexFrondNode = require("FlexFrondNode")
-local RxInstanceUtils = require("RxInstanceUtils")
-local Rx = require("Rx")
 local FrondConstants = require("FrondConstants")
+local RxInstanceUtils = require("RxInstanceUtils")
 
 local FrondManagedUtils = {}
 
 function FrondManagedUtils.mountFrond(instance: GuiBase)
 	-- TODO: 'AbsoluteSize' is delayed a frame, which is just causing pain...
 	local frond = FlexFrondNode.new()
-	frond._maid:GiveTask(RxInstanceUtils.observeProperty(instance, "AbsoluteSize")
-		:Pipe({
-			Rx.distinct(),
-		})
-		:Subscribe(function(absoluteSize: Vector2)
+	frond._maid:GiveTask(
+		RxInstanceUtils.observeProperty(instance, "AbsoluteSize"):Subscribe(function(absoluteSize: Vector2)
 			frond:SetSizingX(FrondConstants.SIZING_PIXEL, absoluteSize.X)
 			frond:SetSizingY(FrondConstants.SIZING_PIXEL, absoluteSize.Y)
-		end))
+		end)
+	)
 	return frond
 end
 
